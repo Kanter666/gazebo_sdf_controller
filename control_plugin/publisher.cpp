89dc395ebd6c527811623853ddfd1efb12e23fe4
Kanter666 <ak2g15@soton.ac.uk>
2018-01-22 00:29:06 +0000

    Switching to object oriented model of robot - subscribers implemented

--- control_plugin/publisher.cpp
+++ control_plugin/publisher.cpp
@@ -1,83 +1,97 @@
-#include <string>
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <gazebo/gazebo_config.h>
-#include <gazebo/transport/transport.hh>
-#include <gazebo/msgs/msgs.hh>
+    #include <string>
+    #include <pybind11/pybind11.h>
+    #include <pybind11/stl.h>
+    #include <gazebo/gazebo_config.h>
+    #include <gazebo/transport/transport.hh>
+    #include <gazebo/msgs/msgs.hh>
+    #include <stdlib.h>
 
-#include <gazebo/gazebo_client.hh>
+    #include <gazebo/gazebo_client.hh>
 
-using namespace std;
+    namespace py = pybind11;
 
-vector<string> jointVel = {};
+    using namespace std;
 
-int publishVector(string topic,  vector<string> values)
-{
-  // Load gazebo as a client
-  gazebo::client::setup();
 
-  // Create our node for communication
-  gazebo::transport::NodePtr node(new gazebo::transport::Node());
-  node->Init();
+    class Interface {
+      public:
+        vector<string> _vectorValue = {};
+        string _name = "";
+        string _jointVelSub = "";
+        string _jointPosSub = "";
 
-  // Publish to the  velodyne topic
-  gazebo::transport::PublisherPtr pub =
-    node->Advertise<gazebo::msgs::GzString_V>(topic);//"~/my_velodyne/vel_cmd"
+      Interface(string robot) {
+        _name = robot;
+        _jointVelSub = "~/" + _name + "/joints_vel";
+        _jointPosSub = "~/" + _name + "/joints_pos";
+      }
 
-  // Wait for a subscriber to connect to this publisher
-  pub->WaitForConnection();
+      void publishVector(string topic,  vector<string> values)
+      {
+        gazebo::transport::NodePtr node(new gazebo::transport::Node());
+        node->Init();
 
-  // Create a a vector3 message
-  gazebo::msgs::GzString_V msg;
+        // Publish to the  velodyne topic
+        gazebo::transport::PublisherPtr pub =
+          node->Advertise<gazebo::msgs::GzString_V>(topic);
 
-  for (int i = 0; i < values.size(); i++)
-    msg.add_data(values[i]);
+        // Wait for a subscriber to connect to this publisher
+        pub->WaitForConnection();
 
-  // Send the message
-  pub->Publish(msg);
+        // Create a a vector3 message
+        gazebo::msgs::GzString_V msg;
 
-  // Make sure to shut everything down.
-  gazebo::client::shutdown();
-}
+        for (int i = 0; i < values.size(); i++)
+          msg.add_data(values[i]);
 
-void cb(ConstGzString_VPtr &_msg)
-{
-  for (int i = 0; i < _msg->data_size(); i++) 
-    jointVel.push_back(_msg->data(i));
-  for (vector<string>::const_iterator i = jointVel.begin(); i != jointVel.end(); ++i)
-    std::cerr << *i << ' ';
-  std::cerr << " were received values \n";
+        // Send the message
+        pub->Publish(msg);
+      }
 
-  // Make sure to shut everything down.
-  gazebo::client::shutdown();
-}
+      vector<string> subscribeVector(string topic){
+        _vectorValue = {};
+        gazebo::client::setup();
 
-vector<string> getJointsVel(string robot)
-{
-  jointVel = {};
-  // Load gazebo as a client
-  gazebo::client::setup();
+        gazebo::transport::NodePtr node(new gazebo::transport::Node());
+        node->Init();
+        // Subscriber
+        gazebo::transport::SubscriberPtr subVector = node->Subscribe(topic, &Interface::cb, this);
+        // Starting delay
+        gazebo::common::Time::MSleep(10);
 
-  // Create our node for communication
-  gazebo::transport::NodePtr node(new gazebo::transport::Node());
-  node->Init();
-  
-  // Subscriber
-  gazebo::transport::SubscriberPtr subJointVel = node->Subscribe("~/" + robot + "/joints_vel", cb);
+        while (_vectorValue.empty()){
+          cout << " loop \n";
+          gazebo::common::Time::MSleep(1);
+        }
 
-  // wait till message is received
-  while(jointVel.empty()){
-  	gazebo::common::Time::MSleep(10);
-  }
-  // wait a little bit longer for message to load
-  gazebo::common::Time::MSleep(10);
+        gazebo::client::shutdown();
 
-  return jointVel;
-}
+        return _vectorValue;
+      }
 
-PYBIND11_MODULE(publisher, m) {
-    m.doc() = "Gazebo publisher"; // optional module docstring
+      void cb(ConstGzString_VPtr &_msg)
+      {
+        _vectorValue = {};
+        for (int i = 0; i < _msg->data_size(); i++) 
+          _vectorValue.push_back(_msg->data(i));
+        cout << "received values \n";
+      }
 
-    m.def("publishVector", &publishVector, "A function which publishes vector data to topic");
-    m.def("getJointsVel", &getJointsVel, "A function which returns velocities of joints");
-}
+      vector<string> getJointsVel()
+      {
+        return subscribeVector(_jointVelSub);
+      }
+
+      vector<string> getJointsPos()
+      {
+        return subscribeVector(_jointPosSub);
+      }
+
+    };
+
+    PYBIND11_MODULE(publisher, m) {
+      py::class_<Interface>(m, "Interface")
+        .def(py::init<const string &>())
+        .def("getJointsVel", &Interface::getJointsVel)
+        .def("getJointsPos", &Interface::getJointsPos);
+    }
